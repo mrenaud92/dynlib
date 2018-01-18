683cff2729b582cc49d6e462e411a49b6fd9ebba
Dave Brown <d.brown@bigdavedev.com>
2018-01-18 20:31:51 +0100

    Address review comments in PR #6
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

--- test/mocks/dlfcn_mock.h
+++ test/mocks/dlfcn_mock.h
@@ -4,13 +4,12 @@
 
 #include <gmock/gmock.h>
 
-class DlfcnMock
-{
-public:
-	DlfcnMock();
-	~DlfcnMock();
-	MOCK_METHOD2(dlopen, void*(char const* filename, int flags));
-	MOCK_METHOD1(dlclose, int(void* handle));
-	MOCK_METHOD2(dlsym, void*(void* handle, char const* symbol));
-	MOCK_METHOD0(dlerror, char*(void));
+class DlfcnMock {
+ public:
+  DlfcnMock();
+  ~DlfcnMock();
+  MOCK_METHOD2(dlopen, void*(char const* filename, int flags));
+  MOCK_METHOD1(dlclose, int(void* handle));
+  MOCK_METHOD2(dlsym, void*(void* handle, char const* symbol));
+  MOCK_METHOD0(dlerror, char*(void));
 };
